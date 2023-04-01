70dee0d033bb3211b766529321f6ee603a9f2fc5
JP-qa1 <jsp_us@yahoo.com>
2023-04-01 11:36:25 -0400

    1st Commit into Git Hub

---
+++ JPMavenGitHubJenkinsTest5/src/test/java/JPMavenGitHubJenkinsTest5pkg/JPTest5.java
@@ -0,0 +1,17 @@
+package JPMavenGitHubJenkinsTest5pkg;
+
+import org.testng.annotations.Test;
+
+public class JPTest5 
+
+{
+	@Test
+	(groups= {"RegressionTest"})
+	
+	public void method5()
+	{
+		System.out.println("Method5 is running");
+		System.out.println("----");
+
+	}
+}
